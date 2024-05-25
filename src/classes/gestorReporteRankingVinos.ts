238e04f8f19a925f2529f963dccfe076021aedf4
valpec <valuupecchio@gmail.com>
2024-05-25 12:18:57 -0300

    hola

---
+++ src/classes/gestorReporteRankingVinos.ts
@@ -0,0 +1,36 @@
+
+class Gestor<T> {
+    private elementos: T[] = [];
+
+    // Método para agregar un elemento
+    agregar(elemento: T): void {
+        this.elementos.push(elemento);
+    }
+
+    generarRankingVinos(){
+
+    }
+    validarPeriodo(){
+
+    }
+    tomarTipoResena(){
+
+    }
+    buscarFormasVisualizacion(){
+
+    }
+    tomarTipoVisualizacion(){
+
+    }
+    tomarConfirmacionGenerarReporte(){
+
+    }
+    
+    buscarVinosEnPeriodoConResenas(periodo:T){
+
+        // for (let i = 0; i < vinos.length; i++) {
+            
+        // }
+    }
+
+}
---
+++ src/classes/pantallaGenerarRankingVinos.ts
@@ -0,0 +1,19 @@
+
+export class Pantalla {
+
+    // private apellido: string;
+    // private imagenPerfil: string;
+    // private nombre: string;
+
+
+    // constructor(apellido: string,  imagenPerfil: string, nombre: string) {
+    //     this.apellido = apellido;
+    //     this.imagenPerfil = imagenPerfil;
+    //     this.nombre = nombre;
+    // }
+
+    generarRankingVinos() {
+    }
+    solicitarFechaDesdeHasta() {
+    } 
+}
